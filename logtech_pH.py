20a4c503e102e9e947ab2dbad937438f94b848f7
plainsweet <130041312+plainsweet@users.noreply.github.com>
2023-05-19 14:19:51 +0700

    Update logtech_pH.py

--- logtech_pH.py
+++ logtech_pH.py
@@ -26,14 +26,25 @@ with st.sidebar.container():
     import json
     import requests
     from streamlit_lottie import st_lottie
-    def load_lottieurl(url: str):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
     
-    lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_XvtPY8hkC5.json")
-    st_lottie(lottie_hello, key="hello")
+    def load_lottiefile(filepath: str):
+        with open(filepath, "r") as f:
+                return json.load(f)
+        
+    lottie_coding = load_lottiefile("lottiefiles/coding.json")
+    st.lottie(
+            lottie_coding,
+            speed=1,
+            reverse=False
+            loop=True,
+            quality="medium",
+            renderer="svg",
+            height=None,
+            width=None,
+            key=None,
+    )
+           
+            
 
 if test == "Home":
     st.title(':blue[Aplikasi penentu pH Larutan]')
